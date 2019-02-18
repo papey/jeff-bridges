55d41df5e31f6ab9bf6ac2b110c594306aca38d3
Wilfried OLLIVIER <wollivier@fdn.fr>
2019-02-18 10:43:40 +0100

    Add argument handling
    Using urfave/cli golang pkg

--- cmd/jb.go
+++ cmd/jb.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
-	fmt.Println("Obviously you’re not a gopher.")
+
+	app := cli.NewApp()
+	app.Name = "jeff-bridges"
+	app.Usage = "Multiply bridges for fun and profit"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add a bridge",
+			Action: func(c *cli.Context) error {
+				fmt.Println("[TODO] Add Bridge")
+				return nil
+			},
+		},
+		{
+			Name:    "delete",
+			Aliases: []string{"d"},
+			Usage:   "delete a bridge",
+			Action: func(c *cli.Context) error {
+				fmt.Println("[TODO] Del Bridge")
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
